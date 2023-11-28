532349b5dcfc9df0ab1cbefa92ac2fd90ff20422
chris wynns <98430976+chriswynns1@users.noreply.github.com>
2023-11-28 04:40:56 +0000

    Update 7 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div>
+    <div className="bg-gradient-to-br from-purple-900 via-indigo-900 to-sky-600 min-h-screen">
       <Navbar />
       <Home />
       <Footer />
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 function Home() {
-    return (
-        <div>
-            Home
-        </div>
-    )
+  return (
+    <div className="text-gray-100">
+      <div className="text-6xl text-center mt-10">Qwizard</div>
+      <div className="text-center text-2xl pt-4">
+        Become the next Qwizard champion!
+      </div>
+      <div className="text-center pt-4">
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            Start
+          </span>
+        </button>
+        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            Sign Up
+          </span>
+        </button>
+      </div>
+      <div className="text-center pt-4">leaderboard goes here</div>
+    </div>
+  );
 }
 
 export default Home;
