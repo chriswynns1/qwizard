46313f5df2e5d880cc443f3c147c70a26e73c261
chriswynns1 <christopher.wynns.521@my.csun.edu>
2023-11-29 16:07:37 -0800

    signup page finished and tested

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth, googleAuthProvider } from './firebase';
 
 function SignUp() {
@@ -7,43 +8,41 @@ function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSignIn = async (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      // Sign in with email and password
-      // You can replace this with your own authentication method
-      // For example, you might want to use signInWithEmailAndPassword
-      const userCredential = await auth.signInWithEmailAndPassword(email, password);
+      // Sign up with email and password
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
 
-      navigate('/dashboard');
-      // Handle successful sign-in (e.g., redirect, update state)
+      navigate('/play');
+      // Handle successful sign-up (e.g., redirect, update state)
     } catch (error) {
-      // Handle sign-in error (e.g., display error message)
-      console.error('Error signing in:', error.message);
+      // Handle sign-up error (e.g., display error message)
+      console.error('Error signing up:', error.message);
     }
   };
 
-  const handleGoogleSignIn = async () => {
+  const handleGoogleSignUp = async () => {
     try {
-      // Sign in with Google using a popup
-      const result = await auth.signInWithPopup(googleAuthProvider);
+      // Sign up with Google using a popup
+      const result = await signInWithPopup(auth, googleAuthProvider);
       const user = result.user;
 
-      navigate('/dashboard');
-      // Handle successful sign-in (e.g., redirect, update state)
+      navigate('/play');
+      // Handle successful sign-up (e.g., redirect, update state)
     } catch (error) {
-      // Handle sign-in error (e.g., display error message)
-      console.error('Error signing in with Google:', error.message);
+      // Handle sign-up error (e.g., display error message)
+      console.error('Error signing up with Google:', error.message);
     }
   };
-
+  
   return (
     <div className="text-gray-100 pt-20 text-center">
       <div className="pt-20 text-3xl">Sign Up</div>
 
       <div className="w-80 mt-10 mx-auto flex flex-col justify-center border rounded-md p-10">
-  <form onSubmit={handleSignIn}>
+  <form onSubmit={handleSignUp}>
     <div className="mb-6">
       <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">
         Your email
@@ -74,12 +73,13 @@ function SignUp() {
     <div className='flex flex-col justify-center'>
       <button
               type="submit"
+              onClick={handleSignUp}
               className="text-white bg-black hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Sign In
+              Sign Up
             </button>
             <button
-            onClick={handleGoogleSignIn}
+            onClick={handleGoogleSignUp}
             className="mt-4 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
           >
             Sign In with Google
