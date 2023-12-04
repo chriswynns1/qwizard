e30a8a19ad47c79d665f2bdea272276c29add444
chriswynns1 <christopher.wynns.521@my.csun.edu>
2023-12-04 14:15:22 -0800

    changed logic to store questions in firestore to reduce amount of api calls

--- src/Category.js
+++ src/Category.js
@@ -4,6 +4,8 @@ import { auth } from './firebase';
 import axios from 'axios';
 import TriviaCategoryCard from "./TriviaCategoryCard";
 import TriviaCard from "./TriviaCard";
+import { getFirestore, doc, setDoc, collection } from 'firebase/firestore';
+
 
 function Category() {
   const [user, setUser] = useState(null);
@@ -29,10 +31,40 @@ function Category() {
     return () => unsubscribe();
   }, []);
 
-  const handleCategoryClick = (category) => {
-    // Set the selected category in the state
-    setSelectedCategory(category);
+  const handleCategoryClick = async (category) => {
+    try {
+      // Fetch trivia questions from the API
+      const response = await axios.get(`https://opentdb.com/api.php?amount=10&category=${category.id}&type=multiple`);
+      const triviaQuestions = response.data.results;
+
+      // Store trivia questions in Firestore
+      await storeQuestionsInFirestore(category.id, triviaQuestions);
+
+      // Set the selected category and navigate to trivia card
+      setSelectedCategory(category);
+    } catch (error) {
+      console.error('Error fetching or storing trivia questions:', error);
+    }
   };
+
+  const storeQuestionsInFirestore = async (categoryId, questions) => {
+    try {
+      const firestore = getFirestore();
+  
+      // Reference to the 'categories' collection
+      const categoriesCollectionRef = collection(firestore, 'categories');
+  
+      // Reference to the document with categoryId in the 'categories' collection
+      const categoryDocRef = doc(categoriesCollectionRef, categoryId.toString()); // Ensure categoryId is a string
+      console.log('categoryDocRef: ', categoryDocRef);
+  
+      // Use setDoc to add or update the document in Firestore
+      await setDoc(categoryDocRef, { questions });
+    } catch (error) {
+      console.error('Error setting trivia questions in Firestore:', error);
+    }
+  };
+  
 
   const handleBackToCategories = () => {
     // Reset the selected category when going back
@@ -41,7 +73,7 @@ function Category() {
 
   return (
     <div className="text-gray-100 pt-20 pb-20">
-      <div className="text-6xl text-center m-5"></div>
+      <div className="text-6xl text-center m-5">Qwizard Trivia</div>
 
       {selectedCategory ? (
         // Display trivia card when a category is selected
