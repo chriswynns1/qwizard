79d15fcf515cd257cd310409977ef2e820ad9894
chris wynns <98430976+chriswynns1@users.noreply.github.com>
2023-12-05 08:11:58 +0000

    Update 3 files

--- src/Category.js
+++ src/Category.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
-import { auth } from './firebase';
-import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
+import axios from "axios";
 import TriviaCategoryCard from "./TriviaCategoryCard";
 import TriviaCard from "./TriviaCard";
-import { getFirestore, doc, setDoc, collection } from 'firebase/firestore';
-
+import { getFirestore, doc, setDoc, collection } from "firebase/firestore";
 
 function Category() {
   const [user, setUser] = useState(null);
@@ -14,12 +13,13 @@ function Category() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get('https://opentdb.com/api_category.php')
+    axios
+      .get("https://opentdb.com/api_category.php")
       .then((response) => {
         setTriviaCategories(response.data.trivia_categories);
       })
       .catch((error) => {
-        console.error('Error fetching trivia categories:', error);
+        console.error("Error fetching trivia categories:", error);
       });
   }, []);
 
@@ -34,7 +34,9 @@ function Category() {
   const handleCategoryClick = async (category) => {
     try {
       // Fetch trivia questions from the API
-      const response = await axios.get(`https://opentdb.com/api.php?amount=10&category=${category.id}&type=multiple`);
+      const response = await axios.get(
+        `https://opentdb.com/api.php?amount=10&category=${category.id}&type=multiple`,
+      );
       const triviaQuestions = response.data.results;
 
       // Store trivia questions in Firestore
@@ -43,34 +45,36 @@ function Category() {
       // Set the selected category and navigate to trivia card
       setSelectedCategory(category);
     } catch (error) {
-      console.error('Error fetching or storing trivia questions:', error);
+      console.error("Error fetching or storing trivia questions:", error);
     }
   };
 
   const storeQuestionsInFirestore = async (categoryId, questions) => {
     try {
       const firestore = getFirestore();
-  
+
       // Reference to the 'categories' collection
-      const categoriesCollectionRef = collection(firestore, 'categories');
-  
+      const categoriesCollectionRef = collection(firestore, "categories");
+
       // Reference to the document with categoryId in the 'categories' collection
-      const categoryDocRef = doc(categoriesCollectionRef, categoryId.toString()); // Ensure categoryId is a string
-      console.log('categoryDocRef: ', categoryDocRef);
-  
+      const categoryDocRef = doc(
+        categoriesCollectionRef,
+        categoryId.toString(),
+      ); // Ensure categoryId is a string
+      console.log("categoryDocRef: ", categoryDocRef);
+
       // Use setDoc to add or update the document in Firestore
       await setDoc(categoryDocRef, { questions });
     } catch (error) {
-      console.error('Error setting trivia questions in Firestore:', error);
+      console.error("Error setting trivia questions in Firestore:", error);
     }
   };
-  
 
   const handleBackToCategories = () => {
     // Reset the selected category when going back
     setSelectedCategory(null);
   };
-
+  console.log("user: ", user);
   return (
     <div className="text-gray-100 pt-20 pb-20">
       <div className="text-6xl text-center m-5">Qwizard Trivia</div>
--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
-import { auth, googleAuthProvider } from './firebase';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { auth, googleAuthProvider } from "./firebase";
 
 function Login() {
-    const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const handleSignIn = async (e) => {
     e.preventDefault();
 
@@ -15,67 +15,78 @@ function Login() {
       await signInWithEmailAndPassword(auth, email, password);
 
       // Redirect or navigate to the desired page upon successful sign-in
-      navigate('/play');
+      navigate("/play");
     } catch (error) {
-      console.error('Error signing in:', error.message);
+      console.error("Error signing in:", error.message);
       // Handle sign-in errors if needed
     }
   };
   return (
-<div className="text-gray-100 pt-20 text-center">
+    <div className="text-gray-100 pt-20 text-center">
       <div className="pt-20 text-3xl">Sign In</div>
 
       <div className="w-80 mt-10 mx-auto flex flex-col justify-center border rounded-md p-10">
-  <form onSubmit={handleSignIn}>
-    <div className="mb-6">
-      <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">
-        Your email
-      </label>
-      <input
-        type="email"
-        id="email"
-        value={email} // Use the value attribute for controlled component
-        onChange={(e) => setEmail(e.target.value)}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="name@email.com"
-        required
-      />
-    </div>
-    <div className="mb-6">
-      <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">
-        Your password
-      </label>
-      <input
-        type="password"
-        id="password"
-        value={password} // Use the value attribute for controlled component
-        onChange={(e) => setPassword(e.target.value)}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        required
-      />
-    </div>
-    <div className='flex flex-col justify-center'>
-      <button
+        <form onSubmit={handleSignIn}>
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-white"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email} // Use the value attribute for controlled component
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="name@email.com"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-white"
+            >
+              Your password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password} // Use the value attribute for controlled component
+              onChange={(e) => setPassword(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <div className="flex flex-col justify-center">
+            <button
               type="submit"
-
               className="text-white bg-black hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Sign Up
             </button>
             <button
-            onClick={() => {
+              onClick={() => {
                 signInWithPopup(auth, googleAuthProvider);
-            }}
-            className="mt-4 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+              }}
+              className="mt-4 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+            >
+              Sign In with Google
+            </button>
+          </div>
+          Need an account?{" "}
+          <a
+            href="/signup"
+            className="hover:text-indigo-300 underline cursor-pointer"
           >
-            Sign In with Google
-          </button>
-    </div>
-    Need an account? <a href="/signup" className='hover:text-indigo-300 underline cursor-pointer'>Sign up</a>
+            Sign up
+          </a>
         </form>
-</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
