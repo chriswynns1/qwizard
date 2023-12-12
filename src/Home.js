bdbc33d9825638b85f966c52fe7eb402b8491d70
anigurgenian1 <112123239+AniGurgenian1@users.noreply.github.com>
2023-12-12 02:55:53 +0000

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -21,7 +21,7 @@ function Home() {
         </div>
         <div className="text-center pt-4">
           <a
-            href="/play"
+            href="/category"
             className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
           >
             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
